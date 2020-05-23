99bab2c19d67da3987184c82883942e3e2030d20
ShreedeviR <64334472+ShreedeviR@users.noreply.github.com>
2020-05-23 14:02:01 -0700

    Add files via upload

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -10,11 +10,13 @@ public class HumanPlayer extends Player implements MouseListener
     public ArrayList<Card> hand = new ArrayList<Card>();
     private int amount;
     private boolean myTurn;
+    BoardPanel board;
     public HumanPlayer (String name, Chips [] chip, Card card, Card card1, Card card2, Card card3, Card card4)
     {
         super (name, chip, card, card1, card2, card3, card4);
+        
         for (Chips ch: chip)
-       
+        {
             if (ch.getColor().equals( Color.RED ))
             {
                 amount += 5;
@@ -31,8 +33,11 @@ public class HumanPlayer extends Player implements MouseListener
             {
                 amount += 100;
             }
-        }
             
+                
+        }
+    }
+    
     
     
     public String addName (String s)
@@ -46,10 +51,28 @@ public class HumanPlayer extends Player implements MouseListener
         myTurn = true;
         return "Your turn";
     }
-    
-    public String getWinMessage()
+    public void bet (int bet)
     {
-        return "You won!";
+        amount = amount - bet;
+        
+    }
+    
+    public String getRoundWinMessage()
+    {
+        return "Yay! You won this round! You now have $" + amount;
+    }
+    public String getWinGameMessage()
+    {
+        return "Congratulations! You have won the entire game!";
+    }
+    public String getLoseGameMessage()
+    {
+        return "You lost the game to computer. You have lost $" + (890 - amount);
+    }
+    public String getLoseRoundMessage ()
+    {
+        return "You lost this round to the computer! Better luck next time. You now have $" + amount +
+                        " Let's start the next round.";
     }
     
     public String getMoneyMessage()
@@ -98,7 +121,8 @@ public class HumanPlayer extends Player implements MouseListener
         {
             return;
         }
-        
+      
+         
     }
     
     
